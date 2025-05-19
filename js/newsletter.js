31273e9890671b8b3e1d9438e2233271daa20563
gionsunday <richardadaji@gmail.com>
2025-05-19 17:50:48 +0100

    correction 2

--- js/newsletter.js
+++ js/newsletter.js
@@ -3,6 +3,7 @@ window.addEventListener("load", () => {
   const newserror = document.querySelector("#newslettererror")
 
   const emailInput = document.querySelector("#newsletteremail");
+    const nameInput = document.querySelector("#newslettername");
 
   newsletterBtn.addEventListener("click", async (e) => {
     e.preventDefault();
@@ -13,17 +14,18 @@ window.addEventListener("load", () => {
       newserror.textContent = "Please enter a valid email!"
     } else {
       newsletterBtn.textContent = "Subscribing...";
-      
       emailInput.style.border = "none"
       
       try {
         const data = await axios.post("https://wcpinitiative.onrender.com/wcpi/api/newsletter", {
           email: emailInput.value,
+          name: nameInput.value
         });
 
         newsletterBtn.textContent = "Subscribed!";
         console.log(data);
         emailInput.value = "";
+        nameInput.value ="" 
         newserror.textContent = data.data.maessage
 
         setInterval(() => {
